19ff9a191df79aafd3bf23ab9fa471bcc0abf8a6
Hask05 <159191978+Hask05@users.noreply.github.com>
2025-05-25 12:45:11 +0200

    music
    .

--- SinLuces-PENGO/src/Game.cpp
+++ SinLuces-PENGO/src/Game.cpp
@@ -72,6 +72,7 @@ AppStatus Game::Initialise(float scale)
     }
     musicStart_Menu = LoadMusicStream("Sounds/Act Clear.ogg");
     musicStage1 = LoadMusicStream("Sounds/Main BGM (Alternate).ogg");
+    musicStage2 = LoadMusicStream("sound/Main BGM (Alternate).ogg");
    /* musicStage2 = LoadMusicStream("sound/music/stage2.ogg");
     musicWin = LoadMusicStream("sound/music/Win.ogg");
     musicLose = LoadMusicStream("sound/music/Lose.ogg");*/
